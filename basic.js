a5bc914f9df645c158ba5a810e9ecc4c293121b5
fajri <fajri@Fajris-iMac.local>
2020-02-21 17:57:04 +0700

    update

--- basic.js
+++ basic.js
@@ -1,20 +1,28 @@
 const pptr = require('puppeteer');
-
 (async() => {
   const browser = await pptr.launch({
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    args: ['--unlimited-storage', '--full-memory-crash-report','--force-gpu-mem-available-mb','--disable-setuid-sandbox'],
+    ignoreHTTPSErrors: true ,
+    dumpio: false,
     headless: true,
+    timeout: 1000000
   });
   const page = await browser.newPage();
   const urls = [];
-  await page.setRequestInterception(true);
-  page.on('request', request => {
-    urls.push(request.url());
-    request.continue();
-  });
 
-  await page.goto('https://wondr.space/milo-pmgm/', {timeout: 10000000,waitUntil: 'networkidle2'})
+    await page.setRequestInterception(true,);
+    page.on('request', request => {
+      urls.push(request.url());
+      request.continue(); 
+    }); 
 
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto('https://wondr.space/milo-pmgm/', {
+        waitUntil: ['load', 'networkidle2'],
+        timeout: 1000000
+    })
+   
   await page.waitForFunction(
       'document.querySelector("body").innerText.includes("PLAY")',{timeout: 6660000}
   );
@@ -22,18 +30,19 @@ const pptr = require('puppeteer');
 
   await page.waitForFunction(
       'document.querySelector("body").innerText.includes("true")',{timeout: 6660000}
-  );
+  );  
   await page.click('button[class="vjs-control vjs-button vjs-custom-opt-button-true"]')
  
-  await page.waitForFunction(
-      'document.querySelector("body").innerText.includes("true")',{timeout: 6660000}
-  );
-  await page.click('button[class="vjs-control vjs-button vjs-custom-opt-button-true"]')
+  // await page.waitForFunction(
+  //     'document.querySelector("body").innerText.includes("true")',{timeout: 6660000}
+  // );
+  // await page.click('button[class="vjs-control vjs-button vjs-custom-opt-button-true"]')
 
-  await page.waitForFunction(
-      'document.querySelector("body").innerText.includes("true")',{timeout: 6660000}
-  );
-  await page.click('button[class="vjs-control vjs-button vjs-custom-opt-button-true"]')
+  // await page.waitForFunction(
+  //     'document.querySelector("body").innerText.includes("true")',{timeout: 6660000}
+  // );
+  // await page.click('button[class="vjs-control vjs-button vjs-custom-opt-button-true"]')
+
   console.log(urls[urls.length-1]);
 
   await browser.close();
